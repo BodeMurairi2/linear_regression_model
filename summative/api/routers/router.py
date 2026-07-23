e382832d2f64530768f8d1af5cc29f5332292e82
bodemurairi2 <b.murairi@alustudent.com>
2026-07-23 20:19:28 +0200

    feat: register retrain router in main API router

--- summative/api/routers/router.py
+++ summative/api/routers/router.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from api.routers.prediction_router import router as prediction_router
+from api.routers.retrain_router import router as retrain_router
 
 def routing(app:FastAPI):
     app.include_router(prediction_router)
+    app.include_router(retrain_router)
