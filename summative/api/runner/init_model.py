88e2ea78f69af910aa95c6d3d54b953d3c2bb152
bodemurairi2 <b.murairi@alustudent.com>
2026-07-23 20:19:28 +0200

    feat: initialize shared RetrainModel instance at app startup

--- summative/api/runner/init_model.py
+++ summative/api/runner/init_model.py
@@ -6,14 +6,19 @@ from pathlib import Path
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
+from api.services.retrain_model import RetrainModel
+
 def init_model():
         """Init model"""
         MODEL_PATH = Path(__file__).parent.parent.parent / "linear_regression" / "model.pkl"
         SCALER_PATH = Path(__file__).parent.parent.parent / "linear_regression" / "scaler.pkl"
 
+        model = joblib.load(filename=MODEL_PATH)
+        scaler = joblib.load(filename=SCALER_PATH)
         return {
-                "model":joblib.load(filename=MODEL_PATH),
-                "scaler":joblib.load(filename=SCALER_PATH)
+                "model":model,
+                "scaler":scaler,
+                "retrain":RetrainModel(model=model, scaler=scaler)
                 }
 
 @asynccontextmanager
